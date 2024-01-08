042360a8811764a9c610a72a27048977260b0bea
626-ju <154496280+626-ju@users.noreply.github.com>
2024-01-08 19:29:53 +0900

    nothing

--- project/start.js
+++ project/start.js
@@ -1,36 +1,20 @@
-const title = document.querySelector(".hello h1:first-child ");
-//1. innerText와 textContent 차이가 뭘까?
-console.dir(title);
-console.log(title);
-//2.console.log를 쓰면 어쩔때는 태그의 모습을 보여주고 <h1 id ="mainTitle">practice</h1>
-//어쩔때는 h1#mainTitle로 보여주는데 왜 그런걸까?
-
-//3.first-child가 뭐야?
-
-
-
-//console.dir(document.querySelector("#mainTitle")); // 메인타이틀이란 아이디 자세히 보기
-//console.log(document.querySelector(".hello")); // 헬로우란 클래스 자세히 보기
+const loginForm = document.querySelector("#loginForm")
+const loginInput = document.querySelector("#loginForm input");
 
 
 
 
-document.querySelector(".hello").addEventListener("click",titleClick);
 
 
-function titleClick(){
-  console.log("title was clicked");
+function loginSubmit(eventOccur) {
+  eventOccur.preventDefault();
+  const userName = loginInput.value;
+  loginForm.classList.add("hidden");
+  document.querySelector("#greeting").innerText = `Hello ${userName}`;
+  document.querySelector("h1").classList.remove('hidden');
+
 }
 
+loginForm.addEventListener('submit', loginSubmit)
 
-
-function titleMouseEnter(){
-  console.log("Entered");
-}
-const titleMouseleave = function(){
-  console.log("leaved");
-}
-
-document.querySelector("#mainTitle").addEventListener("mouseenter", titleMouseEnter);
-
-document.querySelector("#mainTitle").onmouseleave = titleMouseleave;
+//자바스크립트는 이벤트가 실행될때의 정보를 함수의 첫번째 인수로 넣어서 실행시켜준다.
