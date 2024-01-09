ac56ec66ce488c69bad21cb2448763fd21c7b5d4
626-ju <154496280+626-ju@users.noreply.github.com>
2024-01-09 18:37:05 +0900

    파일 추가/로그인/시계
    (복습)안보고 다시해보기.

---
+++ project/Script/Login.js
@@ -0,0 +1,23 @@
+const user_name = localStorage.getItem("userName");
+const loginForm = document.querySelector("form");
+const enteredLogin = document.querySelector("h1")
+
+if (localStorage.getItem("userName") === null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", submitForm);
+} else {
+  doGreeting(user_name);
+}
+
+function submitForm(event) {
+  event.preventDefault;
+  userName = document.querySelector("input").value;
+  localStorage.setItem("userName", userName);
+  loginForm.classList.add("hidden");
+  doGreeting(userName);
+}
+
+function doGreeting(사용자이름) {
+  enteredLogin.classList.remove("hidden");
+  enteredLogin.innerText = `Hello ${사용자이름}`;
+}
