7fa3ee762c8a05015233c62df98a051356aa9af3
626-ju <154496280+626-ju@users.noreply.github.com>
2024-01-09 16:14:10 +0900

    Add files via upload
    함수 내에 변수 설정에 대해 주의할 것.
    ***블록 바깥에서 선언하면 이벤트 실행 전 빈 값이 할당된다.

--- project/start.js
+++ project/start.js
@@ -1,20 +1,36 @@
-const loginForm = document.querySelector("#loginForm")
+const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#loginForm input");
 
-
-
-
-
-
 function loginSubmit(eventOccur) {
+  const userName = loginInput.value; //**블록 바깥에서 선언하면 이벤트가 실행되기 전 빈 값이 할당되어버린다.
+  localStorage.setItem(USER_NAME, userName);
   eventOccur.preventDefault();
-  const userName = loginInput.value;
   loginForm.classList.add("hidden");
-  document.querySelector("#greeting").innerText = `Hello ${userName}`;
-  document.querySelector("h1").classList.remove('hidden');
-
+  doGreeting(userName);
 }
 
-loginForm.addEventListener('submit', loginSubmit)
+function doGreeting(savedUserName) {
+  document.querySelector("#greeting").innerText = `Hello ${savedUserName}`;
+  document.querySelector("h1").classList.remove("hidden");
+}
+
+const USER_NAME = "userName";
+const savedUserName = localStorage.getItem(USER_NAME);//**맨처음엔 빈 값. 그 다음 시행부터 저장된 키 값이 들어온다.
+
+if (savedUserName === null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", loginSubmit);
+} else {
+  doGreeting(savedUserName);
+  //greeting.classList.remove("hidden"); //왜 그냥 greeting이 가능??greeting은 id인데?
+}
+
+
+
+
 
 //자바스크립트는 이벤트가 실행될때의 정보를 함수의 첫번째 인수로 넣어서 실행시켜준다.
+//localStorage.setItem("key", "value")
+//localStorage.getItem("key")  //;
+//localStorage.remove("key") //
+//반복되는 스트링은 변수로 만드는 것이 좋다.
