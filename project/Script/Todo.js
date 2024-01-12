9812046fc3ff1711f43d2730093e40ec7bdbbf1d
626-ju <154496280+626-ju@users.noreply.github.com>
2024-01-12 20:21:47 +0900

    todolist.완성
    여러번 복습하기. 기능이 많아지니까 헷갈린다 ㅠ

--- project/Script/Todo.js
+++ project/Script/Todo.js
@@ -1,64 +1,68 @@
+//1 html에 tofoform 추가 v
+//2 todoForm에 인풋값 제출하기 v
+//3 제출된 인풋 값 로컬스토리지에 저장 v//
+//3-1 새로고침 후 새로운 인풋 값을 넣어도 기존 값에 추가하기//v
+//4 로컬스토리지에 저장된 값 리스트에 추가해서 보여주기 v
+//4-1 즉각즉각 보여주기v
+//5 원하는 항목 리스트에서 지우기(버튼)v
+//6로컬스토리지에서도 지우기
+///인풋되는 값마다 id 제공. 로컬스토리지에서 지울때 id를 통해 제거
+// Date.now()를 통해 고유한 값을 생성할 수 있지만 배열의 길이를 찾아서,1,2,3,4이런 식으로 어레이주면 안될까?//참조 문제땜에 꼬인다.
+
+//------------------------------------------------------------------------------------
 const todoForm = document.querySelector("#todoForm");
 
-const todoList = document.querySelector("#todoList");
+let todoThings = [];
+todoForm.addEventListener("submit", submitInput);
+const savedThings = localStorage.getItem("todoThings");
 
-todoForm.addEventListener("submit", todoSubmit);
+if (savedThings !== null) {
+  todoThings = JSON.parse(savedThings); //여기서 todoThings를 다시 선언하기 위해, 위에서 let 사용.
+  //다시 새로고침을 하더라도 로컬스토리지에 이미 저장된 값이 있으면 그 값을 다시 할당해준다.//todoThings의 초기화 방지
 
-let todos = []; //객체형이기 때문에 const여도 내부의 값 변경 가능, 새로 선언x
-                //배열을 새로 선언하기 위해서는 let으로 해줘야함
-
-const TODOS_KEY = "todos";
-const savedtodos = JSON.parse(localStorage.getItem(TODOS_KEY));
-
-
-
-
-    savedtodos.forEach(v=>createTodoList(v));
-    todos = savedtodos;
-
-//////////////////////////////////////////////////////////////////////
-
-
-
-
-function todoSubmit(eventOccur) {
-  //1.todo리스트 입력 후 제출 하면 발생하는 함수
-  eventOccur.preventDefault();
-  
-  const todoThings = todoForm.querySelector("input").value; //const 사용 유심히 보기
-  //createTodoList(todoThings);
-
-  todoForm.querySelector("input").value = ""; //요소 수정
-  //todoThing =''; //const를 사용해서 변경이 안된다.
-  todos.push(todoThings);
-  saveTodo();
+  todoThings.forEach((v) => createList(v));
 }
 
-function createTodoList(todoThings) {
-  //2.제출된 인풋 값을 리스트에 추가하는 함수
-  //localStorage.setItem("todoThings", todoThings);
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = todoThings;
+//-------------------------------------------------------------------------------------
+function submitInput(event) {
+  event.preventDefault();
+  const submittedInput = todoForm.querySelector("input").value;
+
+  const submittedObj = {
+    id: Date.now(),
+    value: submittedInput,
+  };
+  todoThings.push(submittedObj);
+  saveSubmitted();
+  createList(submittedObj); // 이게 있어야 새로 넣은 인풋 값이 바로바로 업데이트 된다.
+  todoForm.querySelector("input").value = "";
+}
+
+function saveSubmitted() {
+  localStorage.setItem("todoThings", JSON.stringify(todoThings));
+}
+
+function createList(v) {
+  const ul = document.createElement("ul");
+  ul.id = v["id"]; ///.id를 통해 id 추가
+  ul.innerText = v["value"];
+
+  //document.querySelector("li").innerText = v;
+  //근데 참조하는거면 이건 왜 안돼? ///아 아직 도큐먼트에  추가가 안되어서 그렇대
 
   const deleteBtn = document.createElement("button");
-  deleteBtn.addEventListener("click", deleteTodoThing);
+  deleteBtn.innerText = "x";
+  deleteBtn.addEventListener("click", deletList);
 
-  li.appendChild(span);
-  todoList.appendChild(li);
-  li.appendChild(deleteBtn).innerText = "❌";
+  document.querySelector("#todoList").appendChild(ul);
+  ul.appendChild(deleteBtn); // 직접 만든 값에 할당해주기 그래야 forEach루프를 돌떄도 알맞은 위치에 생김.
 }
 
-function deleteTodoThing(eventOccur) {
-  //3.버튼을 누르면 해당 리스트를 지우는 함수
-  todoList.removeChild(eventOccur.target.parentElement);
-  //위 아래 차이 무엇?
-  //const removeLi = eventOccur.target.parentElement;
-  //removeLi.remove();
-  
-}
+function deletList(event) {
+  const removeLi = event.target.parentElement;
 
-function saveTodo() {
-  //4. 제출된 인풋값을 로컬스토리지에 저장하는 함수
-  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+  todoThings = todoThings.filter((v) => v["id"] !== parseInt(removeLi["id"]));
+  saveSubmitted(); //다시 저장해줘야해
+
+  removeLi.remove();
 }
